64f54733a6c1298685a4967744cecac8b14a7371
ivanch167761 <iva.kulikov@gmail.com>
2022-11-04 14:58:06 +0100

    change styling image

--- components/productList.js
+++ components/productList.js
@@ -14,7 +14,7 @@ export const ProductList = ({
       <div className="w-full md:w-1/2 xl:w-1/4 px-4 ">
         <div className="bg-white rounded-lg overflow-hidden mb-10 border border-slate-500">
         <div className="w-full relative">
-          <Image src={productListImg} alt="a"  fill height="auto" style={{ position: 'relative' }}/>
+          <Image src={productListImg} alt="a"  fill style={{ objectFit: 'cover', height: 'auto', position: 'relative' }}/>
         </div>
           <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
             <h3>
