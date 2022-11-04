b2f51e281f22aa64ad07c6ef01a32f4c7c82522f
ivanch167761 <iva.kulikov@gmail.com>
2022-11-04 05:35:48 +0100

    coma added on image rules

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-      domains: ['deeptest.ams3.digitaloceanspaces.com'],
-  },
+      images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.digitaloceanspaces.com',
+      },
+    ],
   swcMinify: true,
   output: 'standalone',
 }
