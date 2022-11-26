842932595623fbc069a99ebeb638d4350029aa82
ivanch167761 <iva.kulikov@gmail.com>
2022-11-26 21:34:30 +0100

    cart screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 // pages/_app.js
-
+import React from 'react'
+import PropTypes from 'prop-types'
 import Layout from '../components/layout'
 import '../styles/globals.css'
 import '../styles/styles.css'
-import { Provider } from 'react-redux';
-import getStore from "../store";
+import { Provider } from 'react-redux'
+import getStore from '../store'
 
-export default function MyApp({ Component, pageProps }) {
-
-  const store = getStore(pageProps.initialState);
+export default function MyApp ({ Component, pageProps }) {
+  const store = getStore(pageProps.initialState)
   return (
   <Provider store={store}>
     <Layout>
@@ -16,4 +16,9 @@ export default function MyApp({ Component, pageProps }) {
     </Layout>
   </Provider>
   )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object
 }
